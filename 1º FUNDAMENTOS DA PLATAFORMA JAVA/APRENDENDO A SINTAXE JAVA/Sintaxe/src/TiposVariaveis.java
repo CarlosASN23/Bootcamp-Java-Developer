a140f2396cb74f77d4c77eaee338ee54c0cec325
Krathor23 <carlosalbertosn.10@gmail.com>
2024-01-02 16:28:44 -0300

    recriação da pasta sintaxe

---
+++ 1º FUNDAMENTOS DA PLATAFORMA JAVA/APRENDENDO A SINTAXE JAVA/Sintaxe/src/TiposVariaveis.java
@@ -0,0 +1,23 @@
+public class TiposVariaveis {
+
+public class TiposVariaveis {
+
+    public static void main(String[] args){
+        
+        double salarioMinimo = 2500;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 87900677;
+        long cpf = 78733645222L; // terminação L - Long 
+        final float pi = 3.14F; // teminação F - float  Final não permite que a
+                                // variavel sofra alterações
+
+        System.out.println(salarioMinimo);
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cpf);
+        System.out.println(cep);
+        
+    }   
+}
+}
