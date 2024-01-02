7d703fcca584af246d161510738444e9eea0b77f
Krathor23 <carlosalbertosn.10@gmail.com>
2024-01-02 16:01:18 -0300

    Algumas observações a respeito de Java Doc

---
+++ 1º FUNDAMENTOS DA PLATAFORMA JAVA/APRENDENDO A SINTAXE JAVA/Java Doc/src/Calculadora.java
@@ -0,0 +1,41 @@
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Carlos Alberto
+* @version 1.0
+* @since   02/01/2024
+*/
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+
+    // One line -
+    // Olá, eu sou um comentário em uma única linha
+
+    // Mult Line
+    /* Olá,
+    * Eu sou um comentario
+    * que posso ser mais detalhadod
+    * quando necessário
+    */
+
+    // Documentação
+    /** 
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+}
