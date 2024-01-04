e46c804d356dba552392e60ba552eafad7ca3bde
Krathor23 <carlosalbertosn.10@gmail.com>
2024-01-04 15:26:17 -0300

    Condicionais compostas

---
+++ 2º ESCOPO E ESTRUTURA DE CONTROLE EM JAVA/INTRODUÇÃO E ESTRUTURAS CONDICIONAIS COM JAVA/CONDICIONAIS COMPOSTAS/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+// ResultadoEscolar.java
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        /*
+         * Algumas vezes, o nosso programa deverá seguir mais
+         * de uma jornada de execução, condionado a uma regra
+         * de negócio, este cenário é demoninado Estrutura Condicional Composta.
+         */
+        
+       int nota = 6;
+       
+       if(nota >= 7)
+        System.out.println("Aprovado");
+
+       else
+        System.out.println("Reprovado");
+    }
+}
