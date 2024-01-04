c17fbd4bb75782d95fb461b85729c9fdb0acc83d
Krathor23 <carlosalbertosn.10@gmail.com>
2024-01-04 15:35:16 -0300

    Switch case

---
+++ 2º ESCOPO E ESTRUTURA DE CONTROLE EM JAVA/INTRODUÇÃO E ESTRUTURAS CONDICIONAIS COM JAVA/SWITCH CASE/src/SistemaMedida.java
@@ -0,0 +1,56 @@
+// SistemaMedida.java
+
+// Modo condicional switch / case
+public class SistemaMedida {
+	public static void main(String[] args) {
+		String sigla = "M";
+
+		switch (sigla) {
+		case "P":{
+			System.out.println("PEQUENO");
+			break;
+		}
+		case "M":{
+			System.out.println("MÉDIO");
+			break;
+		}
+		case "G":{
+			System.out.println("GRANDE");
+			break;
+		}
+		default:
+			System.out.println("INDEFINIDO");
+		}
+		
+		
+		// Modo condicional convencional
+		String plano = "M"; //M / T
+		
+		if(plano == "B") {
+			System.out.println("100 minutos de ligação");
+		}else if(plano == "M") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("WhatsApp e Instagram grátis");	
+		}else if(plano == "T") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("WhatsApp e Instagram grátis");	
+			System.out.println("5Gb Youtube");	
+		}
+
+		// Modo condicional switch/case
+		String tamanho = "M"; // M / T
+
+		switch (tamanho) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+
+	}
+}
